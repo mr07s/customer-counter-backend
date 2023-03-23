3e7b5a539e0a7a43d4d087db622be5cc7ac9f448
mr07s <106861707+mr07s@users.noreply.github.com>
2023-03-23 18:25:07 +0530

    CORS error solved

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ const app = express();
 dotenv.config();
 app.use(express.json({limit:"30mb",extended:true}));
 app.use(express.urlencoded({limit:"30mb",extended:true}));
-app.use(cors());
+app.use(cors({"Access-Control-Allow-Origin":"*"}));
 
 app.get('/',(req,res)=>{
 res.send("This is a customder-counter api ");
 })
 
 app.use('/user',userRoutes);
-app.use('/duedetails',auth,customerRoutes);
-app.use('/addproduct',auth,productroutes);
+app.use('/duedetails',customerRoutes);
+app.use('/addproduct',productroutes);
 
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 
-const connection_Url =process.env.CONNECTION_URL
+const connection_Url ="mongodb+srv://soumya:UbsQsxjlFVkIc4ju@cluster0.grb5anm.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(connection_Url,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(PORT,()=>{console.log(`server is running on port ${PORT}`)}))
