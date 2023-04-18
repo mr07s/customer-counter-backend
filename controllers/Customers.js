7d3ec72418c25d0b395b0dea36cec2aa2f9aed30
mr07s <106861707+mr07s@users.noreply.github.com>
2023-04-18 14:12:54 +0530

    page number and volume number added

--- controllers/Customers.js
+++ controllers/Customers.js
@@ -2,62 +2,60 @@ import mongoose from "mongoose";
 import customer from '../models/customer.js'
 
 
-export const Customer = async(req,res) =>{
-// const {id:userId} =req.params;
-const postcustomerdata =req.body;
-console.log(postcustomerdata);
-const customerdata =new customer(postcustomerdata);
-console.log(customerdata);
-try{
+export const Customer = async (req, res) => {
+    // const {id:userId} =req.params;
+    const postcustomerdata = req.body;
+    console.log(postcustomerdata);
+    const customerdata = new customer(postcustomerdata);
+    console.log(customerdata);
+    try {
 
-await customerdata.save();
-res.status(200).json("Customer details submitted  sucessfully ");
+        await customerdata.save();
+        res.status(200).json("Customer details submitted  sucessfully ");
 
 
-}
-catch(error)
-{
-console.log(error);
-res.status(409).json("Coudn't post a new customerdata sucessfully");
+    }
+    catch (error) {
+        console.log(error);
+        res.status(409).json("Coudn't post a new customerdata sucessfully");
 
-}
-  
+    }
+
 };
 
 
-export const getcustomerdetails =  async(req,res)=>{
-try{
-    // const user =req.body;
-        
-    const customerdetails = await customer.find()
-    res.status(200).json(customerdetails);
-    
-    
+export const getcustomerdetails = async (req, res) => {
+    try {
+        // const user =req.body;
+
+        const customerdetails = await customer.find()
+        res.status(200).json(customerdetails);
+
+
     }
-    catch(error)
-{        
-res.status(404).json({message:error.message});
+    catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+
+
+
+
+
 }
-    
-    
-    
-    
-    
-}
-export const deleteCustomer= async(req,res)=>{
+export const deleteCustomer = async (req, res) => {
     // console.log(req);
     // let {id:_id} = req.body;
-    const {id:_id} = req.params;
+    const { id: _id } = req.params;
 
     // console.log(_id);
-    try{
+    try {
         await customer.findByIdAndRemove(_id)
         // console.log(deleteData)
         // const customerdetails = await customer.find()
-        res.status(200).json({message:"Sucessfully deleted"});
-    
+        res.status(200).json({ message: "Sucessfully deleted" });
+
     }
-    catch(error){
+    catch (error) {
         res.send(error);
         // console.log("Customer deletion unsuccessfull due to "+error.message);
     }
@@ -69,29 +67,29 @@ export const deleteCustomer= async(req,res)=>{
 
 
 
-    export const editCustomer =async(req,res)=>{
-        const {id:_id} = req.params;
-        const{Name,undertakenby, price,purchasingdate,duedate,paidamount}=req.body;
-        try{
-            await customer.findByIdAndUpdate(_id,{
-                $set:{Name:Name,undertakenby:undertakenby,undertakenby:undertakenby,price:price,purchasingdate:purchasingdate,duedate:duedate,paidamount:paidamount},
-                // $set:{undertakenby:undertakenby},
-                // $set:{price:price},
-                // $set:{purchasingdate:purchasingdate},
-                // $set:{duedate:duedate},
-            });
-            
-            return res.status(200).json({message:"yahhh!updated succesfully"})
-        }
-        catch(e){
-            console.log(e);
-            return res.status(404).send(e);    
-        }
+export const editCustomer = async (req, res) => {
+    const { id: _id } = req.params;
+    const { Name, undertakenby, price, purchasingdate, duedate, paidamount, volume, pages } = req.body;
+    try {
+        await customer.findByIdAndUpdate(_id, {
+            $set: { Name: Name, undertakenby: undertakenby, undertakenby: undertakenby, price: price, purchasingdate: purchasingdate, duedate: duedate, paidamount: paidamount, volume, pages },
+            // $set:{undertakenby:undertakenby},
+            // $set:{price:price},
+            // $set:{purchasingdate:purchasingdate},
+            // $set:{duedate:duedate},
+        });
 
-
+        return res.status(200).json({ message: "yahhh!updated succesfully" })
     }
-    
-    
+    catch (e) {
+        console.log(e);
+        return res.status(404).send(e);
+    }
+
+
+}
+
+
 
 
 
--- models/customer.js
+++ models/customer.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 
 const customerSchema = mongoose.Schema({
-// id:{type:String,required:true},
-// SerialNo:{type:String,required:true},
-// Name:{type:String,required:true},
-// undertakenby:{type:String,required:true},
-// price:{type:String,required:true},
-// purchasingdate:{type:String,required:true},
-// duedate:{type:String,required:true}
+    // id:{type:String,required:true},
+    // SerialNo:{type:String,required:true},
+    // Name:{type:String,required:true},
+    // undertakenby:{type:String,required:true},
+    // price:{type:String,required:true},
+    // purchasingdate:{type:String,required:true},
+    // duedate:{type:String,required:true}
 
 
-Name:{type:String},
-undertakenby:{type:String},
-price:{type:String},
-purchasingdate:{type:String},
-duedate:{type:String},
-paidamount:{type:String},
-userId:{type:String}
+    Name: { type: String },
+    undertakenby: { type: String },
+    price: { type: String },
+    purchasingdate: { type: String },
+    duedate: { type: String },
+    paidamount: { type: String },
+    volume: { type: String },
+    pages: { type: String },
+    userId: { type: String }
 
 
 
 })
-export default mongoose.model("customer",customerSchema);
+export default mongoose.model("customer", customerSchema);
